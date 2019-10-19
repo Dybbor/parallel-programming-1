4d5d6b3394e548b785fd6f38138d5ce3bb6fe903
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2019-10-19 22:54:26 +0300

    lab done but need to clean code for terminal class

--- 1706-4/dobrohotov_vn/Lab 1/Lab 1/Source.cpp
+++ 1706-4/dobrohotov_vn/Lab 1/Lab 1/Source.cpp
@@ -20,6 +20,15 @@ void PrintVector(int *x, int size)
 	for (int i = 0; i < size; i++)
 		cout << x[i] << "  ";
 }
+void PrintInfo(int rank, int size, int block, int rows, int cols, int *tmp, int *local_max)
+{
+	cout << "\nInformation\nProcess " << rank << "\n" << "Size " << size << "\n" << "block " << block << "\n" << "rows " << rows << "\n" << "cols " << cols << endl;
+
+	for (int i = 0; i < rows*block; i++)
+		cout << " tmp " << rank << " " << tmp[i] << " ";
+	for (int i = 0; i < block; i++)
+		cout << "local max " << rank << " " << local_max[i] << " ";
+}
 void PrintMatrix(int *vec, int rows, int cols)
 {
 	if (rows < 100 && cols < 100) {
@@ -34,7 +43,7 @@ void PrintMatrix(int *vec, int rows, int cols)
 	else
 		cout << "Matrix too big" << endl;
 }
-void Tranpose(int* vec, int &rows, int &cols)
+void Tranpose(int* vec, int rows, int cols)
 {
 	int* copy = new int[rows*cols];
 	for (int i = 0; i < rows*cols; i++)
@@ -62,10 +71,8 @@ void Max(int *mas, int *res, int cols, int pos)
 	res[pos] = max;
 }
 
-
-int main(int argc, char** argv)
+int main(int argc, char ** argv)
 {
-
 	int size; //количесво процессов
 	int rank; //индентификатор процесса
 	int rows, cols; //строкиб колонки
@@ -73,7 +80,7 @@ int main(int argc, char** argv)
 		start_pp = 0, end_pp = 0;
 	int* vec = NULL; //Матрица в виде вектора
 	int* resl = NULL; //Результирующий вектор максимумов для последовательного
-	int* resp = NULL; //Результирующий вектор максимумов для параллейного
+	int *resp;//=NULL; //= NULL; //Результирующий вектор максимумов для параллейного
 	int *tmp = NULL; //Локальный массив для блока
 	int *local_max = NULL; //здесь будут храниться локальные максимумы
 	int block; //Количество столбцов в блоке
@@ -99,72 +106,84 @@ int main(int argc, char** argv)
 			else
 				break;
 		}
+		if (cols < size) //Если колонок меньше чем процессов, не задействовать все процессы, а столько сколько нужно
+			size = cols;
 		vec = InitMatrix(rows, cols); //инциализируется матрица в виде вектора рандомно
 		block = cols / size; //в блоке n-ое кол во столбцов
-		tmp = new int[cols*block];	// промежуточный массив для частей вектора
+		left = cols % size;  //остаточные столбцы которые не попадут на процессы
+		tmp = new int[rows*block];	// промежуточный массив для частей вектора
 		local_max = new int[block];
 		resl = new int[cols];	// массив в котором будут хранится максимальные значения
-		resp = new int[cols];	// массив в котором будут хранится максимальные значения
-		left = cols % size;  //остаточные столбцы которые не попадут на процессы
+		//resp = new int[cols];	// массив в котором будут хранится максимальные значения
+		/*resp = new vector <int>();
+		resp->resize(cols);*/
+		PrintMatrix(vec, rows, cols);
 		Tranpose(vec, rows, cols); //Транспонирование матрицы (переделывание вектора)
-		//Последовательный алгоритм
-		start_linear = MPI_Wtime();
-		int max = vec[0];
-		int k = 0;
-		for (int i = 1; i < rows*cols; i++)
-		{
-			if (max < vec[i])
-				max = vec[i];
-			if (i % rows == rows - 1) {
-				resl[k] = max;
-				k++;
-				max = vec[++i];
-			}
-		}
-		end_linear = MPI_Wtime();
-		PrintVector(resl, cols);
-		//Паралельный алгоритм
-		start_pp = MPI_Wtime();
-		//MPI_Bcast(&block, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		MPI_Bcast(&cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		MPI_Scatter(vec, cols*block, MPI_INT, tmp, cols*block, MPI_INT, 0, MPI_COMM_WORLD);
-		int c = 0;
-		for (int i = 0; i < block; i++) {
-			Max(tmp + c, local_max, cols, i);
-			c += cols;
-		}
 	}
-	else
+	MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&block, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	resp = new int[cols];	// массив в котором будут хранится максимальные значения
+	if (rank > 0)
 	{
-		cols = 0;
-		block = 0;
-		tmp = new int[cols*block];	// промежуточный массив для частей вектора
-		resp = new int[cols];	// массив в котором будут хранится максимальные значения
+		tmp = new int[rows*block];	// промежуточный массив для частей вектора
+		//resp = new int[cols];	// массив в котором будут хранится максимальные значения
+		/*resp = new vector <int>();
+		resp->resize(cols);*/
 		local_max = new int[block];
-		int c = 0;
-		for (int i = 0; i < block; i++) {
-			Max(tmp + c, local_max, cols, i);
-			c += cols;
-		}
 	}
-	if (rank == 0)
+	MPI_Scatter(vec, rows*block, MPI_INT, tmp, rows*block, MPI_INT, 0, MPI_COMM_WORLD);
+	//Поиск максимума по столбцам и занесение его в массив локальных сумм
+	int c = 0; //Смещение
+	for (int i = 0; i < block; i++)
 	{
-		MPI_Gather(local_max, 3, MPI_INT, resp, 3, MPI_INT, 0, MPI_COMM_WORLD);
+		int max = tmp[0 + c];
+		for (int j = 1; j < rows; j++)
+		{
+			if (tmp[j + c] > max)
+				max = tmp[j + c];
+		}
+		c += rows;
+		local_max[i] = max;
+	}
+	PrintInfo(rank, size, block, rows, cols, tmp, local_max);
+
+	MPI_Gather(local_max, block, MPI_INT, resp, block, MPI_INT, 0, MPI_COMM_WORLD);
+	if (rank == 0)
+		for (int i = 0; i < cols; i++)
+			cout << " max vector" << resp[i] << " ";
+	//Считаются столбцы которые не попали на процессы
+	if (rank == 0)
 		if (left != 0)
+		{
+			int c = 0; //Смещение
 			for (int i = 0; i < left; i++)
 			{
-
+				int max = vec[rows*size + c];
+				for (int j = 1; j < rows; j++)
+				{
+					if (vec[j + rows * size + c] > max)
+						max = vec[j + rows * size + c];
+				}
+				c += rows;
+				resp[i + size] = max;
 			}
-		cout << "PP" << endl;
-		PrintVector(resp, cols);
+		}
+	if (rank == 0)
+	{
+		cout << "\nafter" << endl;
+		for (int i = 0; i < cols; i++)
+			cout << " max vector" << resp[i] << " ";
 	}
-	end_pp = MPI_Wtime();
+	//MPI_Barrier(MPI_COMM_WORLD);
 
+	/*cout << "Process " << rank << endl;
+	cout << " Size " << size << endl;
+	cout << "Block " << block;*/
 	delete[]vec;
 	delete[]resl;
-	delete[]resp;
+	//delete[]resp;
+
 	MPI_Finalize();
-	return 0;
-
 }
-
