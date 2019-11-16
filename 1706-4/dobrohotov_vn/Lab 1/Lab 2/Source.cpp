289a9aa4797347a1c2e8a9806cb4477e31812ced
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2019-11-16 18:51:51 +0300

    look like done

--- 1706-4/dobrohotov_vn/Lab 1/Lab 2/Source.cpp
+++ 1706-4/dobrohotov_vn/Lab 1/Lab 2/Source.cpp
@@ -2,20 +2,17 @@
 #include "mpi.h"
 #include <queue>
 #include <windows.h>
-using namespace std;
-
 
 const int READER_OPEN = 0;
 const int READER_CLOSE = 1;
 const int WRITER_OPEN = 2;
 const int WRITER_CLOSE = 3;
 
-const int RESPONCE_READER_YES = 4;
-const int RESPONCE_READER_WAIT = 5;
-const int RESPONCE_WRITER_YES = 6;
-const int RESPONCE_WRITER_NO = 7;
-const int RESPONCE_WRITER_WAIT = 8;
-
+const int RESPONSE_READER_YES = 4;
+const int RESPONSE_READER_WAIT = 5;
+const int RESPONSE_WRITER_YES = 6;
+const int RESPONSE_WRITER_NO = 7;
+const int RESPONSE_WRITER_WAIT = 8;
 
 struct Request
 {
@@ -23,8 +20,7 @@ struct Request
 	int step;
 };
 
-
-void RunReader(int rank) 
+void RunReader(int rank,int time=1000) 
 {
 	MPI_Status status;
 	Request request;
@@ -33,14 +29,14 @@ void RunReader(int rank)
 	request.step = READER_OPEN;
 	MPI_Send(&request, 2, MPI_INT, 0, 1, MPI_COMM_WORLD);
 	MPI_Recv(&response, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-	if (response == RESPONCE_READER_WAIT)
+	if (response == RESPONSE_READER_WAIT)
 	{
 		MPI_Recv(&response, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 		std::cout << "process " << rank << " - reader wait succeed" << std::endl;
 	}
 	std::cout << "process " << rank<< " - reader open succeed" << std::endl;
 	///
-	Sleep(1000);
+	Sleep(time);
 	//////
 	request.step = READER_CLOSE;
 	MPI_Send(&request, 2, MPI_INT, 0, 1, MPI_COMM_WORLD);
@@ -48,7 +44,7 @@ void RunReader(int rank)
 	std::cout << "process " << rank << " - reader finish" << std::endl;
 }
 
-void RunWriter(int rank) 
+void RunWriter(int rank,int time=1000) 
 {
 	MPI_Status status;
 	Request request;
@@ -57,16 +53,15 @@ void RunWriter(int rank)
 	request.step = WRITER_OPEN;
 	MPI_Send(&request, 2, MPI_INT, 0, 1, MPI_COMM_WORLD);
 	MPI_Recv(&response, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-	if (response == RESPONCE_WRITER_WAIT) 
+	if (response == RESPONSE_WRITER_WAIT) 
 	{
 		MPI_Recv(&response, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 		std::cout << "process " << rank << " - writer wait succeed" << std::endl;
 	}
 	std::cout << "process " << rank << " - writer open succeed" << std::endl;
 	////
-	Sleep(1000);
+	Sleep(time);
 	//////
-
 	request.step = WRITER_CLOSE;
 	MPI_Send(&request, 2, MPI_INT, 0, 1, MPI_COMM_WORLD);
 	MPI_Recv(&response, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
@@ -85,13 +80,19 @@ int main(int argc, char**argv)
 	{
 		int countOfReaders=0;
 		int countOfWriters=0;
-		queue <int> q_readers,q_writers;
+		std::queue <int> q_readers,q_writers;
 		bool waitingWriter = false;
-		for (int i = 0; i < k * 2; i++) 
+		int numb = 0;
+		for (int i = 0; i < 12; i++) 
 		{
 			MPI_Status status;
 			Request request;
 			MPI_Recv(&request, 2, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+			std::cout << "\nSTatus" <<std:: endl;
+			std::cout << "reader " << countOfReaders << " writer " << countOfWriters << std::endl;
+			std::cout << "wait reader size " << q_readers.size() << "  wait writer size " << q_writers.size() << std::endl;
+			std::cout << "\n\t\t "<<numb<<" NEW REQUEST" << std::endl;
+			numb++;
 			switch (request.step)
 			{
 			case READER_OPEN:
@@ -99,27 +100,29 @@ int main(int argc, char**argv)
 				if (countOfWriters == 0)
 				{
 					countOfReaders++;
-					std::cout << " Manager: process " <<request.rank<< " open reader - yes" << std::endl;
-					MPI_Send(&RESPONCE_READER_YES, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
+					std::cout << "\t\t  Manager: process " <<request.rank<< " open reader - yes\n" << std::endl;
+					MPI_Send(&RESPONSE_READER_YES, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
 				}
 				else 
 				{
-					std::cout << " Manager: process " << request.rank << " open reader - no" << std::endl;
+					std::cout << " \t\t Manager: process " << request.rank << " open reader - no\n" << std::endl;
 					q_readers.push(request.rank);
-					MPI_Send(&RESPONCE_READER_WAIT, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
+					MPI_Send(&RESPONSE_READER_WAIT, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
 				}
 				break;
 			}
 			case READER_CLOSE:
 			{
 					countOfReaders--;
-					std::cout << " Manager: process " << request.rank << " close reader - yes" << std::endl;
-					MPI_Send(&RESPONCE_READER_YES, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
+					std::cout << " \t\t Manager: process " << request.rank << " close reader - yes\n" << std::endl;
+					MPI_Send(&RESPONSE_READER_YES, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
 					if (q_writers.size() > 0 && countOfReaders==0) 
 					{
+						std::cout << "\t\t \nQueue writer start\n" << std::endl;
 						for (int i = 0; i < q_writers.size(); i++)
 						{
-							MPI_Send(&RESPONCE_WRITER_YES, 1, MPI_INT, q_writers.front(), 0, MPI_COMM_WORLD);
+							countOfWriters++;
+							MPI_Send(&RESPONSE_WRITER_YES, 1, MPI_INT, q_writers.front(), 0, MPI_COMM_WORLD);
 							q_writers.pop();
 						}
 					}
@@ -130,27 +133,41 @@ int main(int argc, char**argv)
 				if (countOfReaders == 0 && countOfWriters==0)
 				{
 					countOfWriters++;
-					std::cout << " Manager: process " << request.rank << " open writer - yes" << std::endl;
-					MPI_Send(&RESPONCE_WRITER_YES, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
+					std::cout << "\t\t  Manager: process " << request.rank << " open writer - yes\n" << std::endl;
+					MPI_Send(&RESPONSE_WRITER_YES, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
 				}
 				else 
 				{
-					std::cout << " Manager: process " << request.rank << " open writer - no" << std::endl;
+					std::cout << "\t\t  Manager: process " << request.rank << " open writer - no\n" << std::endl;
 					q_writers.push(request.rank);
-					MPI_Send(&RESPONCE_WRITER_WAIT, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
+					MPI_Send(&RESPONSE_WRITER_WAIT, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
 				}
 				break;
 			}
 			case WRITER_CLOSE:
 			{
 				countOfWriters--;
-				std::cout << " Manager: process " << request.rank << " close writer - yes" << std::endl;
-				MPI_Send(&RESPONCE_WRITER_YES, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
-				if (q_readers.size() > 0)
+				std::cout << "\t\t  Manager: process " << request.rank << " close writer - yes\n" << std::endl;
+				MPI_Send(&RESPONSE_WRITER_YES, 1, MPI_INT, request.rank, 0, MPI_COMM_WORLD);
+
+				if (q_writers.size() > 0 && countOfReaders == 0)
 				{
-					for (int i = 0; i < q_readers.size(); i++) 
+					std::cout << "\nQueue writer start\n" << std::endl;
+					for (int i = 0; i < q_writers.size(); i++)
 					{
-						MPI_Send(&RESPONCE_READER_YES, 1, MPI_INT, q_readers.front(), 0, MPI_COMM_WORLD);
+						countOfWriters++;
+						MPI_Send(&RESPONSE_WRITER_YES, 1, MPI_INT, q_writers.front(), 0, MPI_COMM_WORLD);
+						q_writers.pop();
+					}
+				}
+				else if (q_readers.size() > 0)
+				{
+					std::cout << "\nQueue reader start\n" << std::endl;
+					int q_size = q_readers.size();
+					for (int i = 0; i < q_size; i++)
+					{
+						countOfReaders++;
+						MPI_Send(&RESPONSE_READER_YES, 1, MPI_INT, q_readers.front(), 0, MPI_COMM_WORLD);
 						q_readers.pop();
 					}
 				}
@@ -159,7 +176,23 @@ int main(int argc, char**argv)
 			}
 		}
 	}
-	if (rank % 2 ==0 && rank!=0)
+	if (rank == 1) 
+	{
+		RunWriter(rank, 1000);
+		RunReader(rank, 500);
+
+	}
+	if (rank == 2) 
+	{
+		RunWriter(rank, 1000);
+		RunReader(rank, 500);
+	}
+	if (rank == 3)
+	{
+		RunReader(rank, 500);
+		RunReader(rank, 500);
+	}
+	/*if (rank % 2 ==0 && rank!=0)
 	{
 		for (int i = 0; i < k; i++) 
 		{
@@ -172,7 +205,7 @@ int main(int argc, char**argv)
 		{
 			RunWriter(rank);
 		}
-	}
+	}*/
 	MPI_Finalize();
 	return 0;
 }
