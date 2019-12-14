ea76ec6a23fe0df49e0b15cba8249c2b3ad253df
Dybbor <19dobrohotov99@mail.ru>
2019-12-14 12:39:04 +0300

    add linear algorithm in mpi cpp

--- 1706-4/dobrohotov_vn/Lab 1/Lab 3/Source.cpp
+++ 1706-4/dobrohotov_vn/Lab 1/Lab 3/Source.cpp
@@ -1,60 +1,154 @@
-//#include <iostream>
-//#include <opencv2/opencv.hpp>
-//#include "mpi.h"
-//#include <cmath>
-//using namespace cv;
-//using namespace std;
-//double** createGauseKerel(double **kernel,double sigma)
-//{
-//	int radius = 1;
-//	double norm = 0;
-//	for (int i = -radius; i <=radius; ++i) 
-//	{
-//		for (int j = -radius; j <= radius; ++j)
-//		{
-//			kernel[i + radius][j + radius] = (double)(exp(-(i*i + j * j) / sigma * sigma));
-//			norm += kernel[i + radius][j + radius];
-//		}
-//	}
-//	cout <<"Norm " <<norm << endl;
-//	for (int i = 0; i < 3; i++)
-//		for (int j = 0; j < 3; j++)
-//			kernel[i][j] /= norm;
-//	return kernel;
-//}
-//
-//
-//int main(int argc,char**argv) 
-//{
-//	//Initialize
-//	int rank, size;
-//	Mat image;
-//	//Start program
-//	MPI_Init(&argc, &argv);
-//	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-//	MPI_Comm_size(MPI_COMM_WORLD, &size);
-//	if (size == 1) {
-//		cout << "Need more process then 1" << endl;
-//		MPI_Finalize();
-//		return -1;
-//	}
-//	if (rank == 0) 
-//	{
-//		image = imread("D:\\GitProject\\parallel-programming-1\\1706-4\\dobrohotov_vn\\Lab 1\\Picture\\cat.jpg");
-//		namedWindow("Image", WINDOW_NORMAL);
-//		if (!image.data) {
-//			cout << "Error load image" << endl;
-//			MPI_Finalize();
-//			return - 2;
-//		}
-//		if (image.channels()>1) 
-//		{
-//			cvtColor(image, image, COLOR_BGR2GRAY);
-//			cout << "image to grey color" << endl;
-//		}
-//	}
-//	MPI_Finalize();
-//	imshow("Image", image);
-//	waitKey();
-//	return 0;
-//}
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "mpi.h"
+#include <cmath>
+using namespace cv;
+using namespace std;
+
+#define M_PI 3.14159265358979323846
+double* createKernel(double* kernel, double sigma)
+{
+	double sum = 0;
+	for (int i = -1; i <= 1; i++)
+	{
+		for (int j = -1; j <= 1; j++)
+		{
+			kernel[i + j + 2 * (i + 2)] = (double)(exp(-(i*i + j * j) / (sigma*sigma))) *(1 / (sigma*sqrt(2 * M_PI)));
+			sum += kernel[i + j + 2 * (i + 2)];
+		}
+	}
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {
+			kernel[i + j + 2 * (i + 2)] /= sum;
+		}
+	}
+	return kernel;
+}
+
+Mat duplicateBorder(Mat image)
+{
+
+	Mat new_image(image.rows + 2, image.cols + 2, CV_8UC1);
+	for (int i = 0; i < new_image.rows; i++)
+		for (int j = 0; j < new_image.cols; j++)
+			new_image.at<uchar>(i, j) = 128;
+	new_image.at<uchar>(0, 0) = image.at<uchar>(0, 0);
+	new_image.at<uchar>(0, new_image.cols - 1) = image.at<uchar>(0, image.cols - 1);
+	new_image.at<uchar>(new_image.rows - 1, 0) = image.at<uchar>(image.rows - 1, 0);
+	new_image.at<uchar>(new_image.rows - 1, new_image.cols - 1) = image.at<uchar>(image.rows - 1, image.cols - 1);
+	for (int i = 0; i < image.cols; i++)
+	{
+		new_image.at<uchar>(0, i + 1) = image.at<uchar>(0, i);
+		new_image.at<uchar>(new_image.rows - 1, i + 1) = image.at<uchar>(image.rows - 1, i);
+	}
+	for (int i = 0; i < image.rows; i++)
+	{
+		new_image.at<uchar>(i + 1, 0) = image.at<uchar>(i, 0);
+		new_image.at<uchar>(i + 1, new_image.cols - 1) = image.at<uchar>(i, image.cols - 1);
+	}
+	for (int i = 0; i < image.rows; i++)
+		for (int j = 0; j < image.cols; j++)
+		{
+			new_image.at<uchar>(i + 1, j + 1) = image.at<uchar>(i, j);
+		}
+
+	return new_image;
+}
+uchar*  processImage(uchar* data, double* kernel, int rows, int cols)
+{
+	uchar *res_data = new uchar[(rows - 2)*(cols - 2)];
+	int count_res_data = 0;
+	int move = 0;
+	cout << (rows)*(cols)-2 * (cols)-1 << " ind" << endl;
+	for (int i = 0; i < (rows)*(cols)-2 * (cols)-1; i++)
+	{
+		int res = 0;
+		move = 0;
+		//cout << i;
+		int count_kernel = 0;
+		for (int j = 0; j < 3; j++)
+		{
+			for (int k = 0; k < 3; k++)
+			{
+				//cout <<"   "<<(int)new_image.data[i + j + k + move] << " ";
+				res += data[i + j + k + move] * kernel[count_kernel];
+				count_kernel++;
+			}
+			move += (cols)-1;
+		}
+		if (res > 255)
+			res = 255;
+		if (res < 0)
+			res = 0;
+		res_data[count_res_data] = res;
+		count_res_data++;
+		if ((i + 3) % (cols) == 0)
+			i += 2;
+	}
+	return res_data;
+}
+
+
+int main(int argc,char**argv) 
+{
+	//Initialize
+	int rank, size;
+	Mat image,copy,res;
+	uchar* data = nullptr;
+	//Start program
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	double start_linear;
+	double finish_linear;
+	double start_pp;
+	double finish_pp;
+	double start_duplicate;
+	double finish_duplicate;
+	double kernel[9] = { 1,2,1,2,4,2,1,2,1 };
+
+	if (size == 1) {
+		cout << "Need more process then 1" << endl;
+		MPI_Finalize();
+		return -1;
+	}
+	if (rank == 0) 
+	{
+		image = imread("D:\\GitProject\\parallel-programming-1\\1706-4\\dobrohotov_vn\\Lab 1\\Picture\\test1.jpg");
+		if (!image.data) {
+			cout << "Error load image" << endl;
+			MPI_Finalize();
+			return - 2;
+		}
+		if (image.channels()>1) 
+		{
+			cvtColor(image, image, COLOR_BGR2GRAY);
+			cout << "image to grey color" << endl;
+		}
+		//Initialize image
+		res = image.clone();
+		//normirovvka intensity
+		for (int i = 0; i < 9; ++i)
+			kernel[i] /= 16;
+		//dublicate
+		start_duplicate = MPI_Wtime();
+		copy = duplicateBorder(image);
+		finish_duplicate = MPI_Wtime();
+		//linear_algorithm
+		start_linear = MPI_Wtime();
+		res.data = processImage(copy.data, kernel, copy.rows, copy.cols);
+		finish_linear = MPI_Wtime();
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Finalize();
+	namedWindow("Image", WINDOW_NORMAL);
+	namedWindow("copy", WINDOW_NORMAL);
+	namedWindow("res", WINDOW_NORMAL);
+	imshow("Image", image);
+	imshow("copy", copy);
+	imshow("res", res);
+	cout << "dublicate time " << finish_duplicate - start_duplicate << endl;
+	cout << "linear time " << finish_linear - start_linear << endl;
+	waitKey();
+	return 0;
+}
--- 1706-4/dobrohotov_vn/Lab 1/Lab 3/Source1.cpp
+++ 1706-4/dobrohotov_vn/Lab 1/Lab 3/Source1.cpp
@@ -1,130 +1,161 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <mpi.h>
-using namespace cv;
-using namespace std;
-#define M_PI 3.14159265358979323846
-Mat filterGauss(Mat image)
-{
-	return image;
-}
-
-double* createKernel(double* kernel, double sigma)
-{
-	double sum = 0;
-	for (int i = -1; i <= 1; i++)
-	{
-		for (int j = -1; j <= 1; j++)
-		{
-			kernel[i + j + 2 * (i + 2)] = (double)(exp(-(i*i + j * j) / (sigma*sigma))) *(1/ (sigma*sqrt(2 * M_PI)));
-			sum += kernel[i + j + 2 * (i + 2)];
-		}
-	}
-	for (int i = -1; i <= 1; i++) {
-		for (int j = -1; j <= 1; j++) {
-			kernel[i + j + 2 * (i + 2)] /= sum;
-		}
-	}
-	return kernel;
-}
-
-Mat duplicateBorder(Mat image) 
-{
-
-	Mat new_image(image.rows + 2, image.cols + 2, CV_8UC1);
-	for (int i = 0; i < new_image.rows; i++)
-		for (int j = 0; j < new_image.cols; j++)
-			new_image.at<uchar>(i, j) = 128;
-	new_image.at<uchar>(0, 0) = image.at<uchar>(0, 0);
-	new_image.at<uchar>(0, new_image.cols-1) = image.at<uchar>(0, image.cols-1);
-	new_image.at<uchar>(new_image.rows-1, 0) = image.at<uchar>(image.rows-1, 0);
-	new_image.at<uchar>(new_image.rows-1,new_image.cols-1) = image.at<uchar>(image.rows-1, image.cols-1);
-	for (int i = 0; i < image.cols; i++)
-	{
-		new_image.at<uchar>(0, i + 1) = image.at<uchar>(0, i);
-		new_image.at<uchar>(new_image.rows - 1, i + 1) = image.at<uchar>(image.rows - 1, i);
-	}
-	for (int i = 0; i < image.rows; i++)
-	{
-		new_image.at<uchar>(i+1, 0) = image.at<uchar>(i, 0);
-		new_image.at<uchar>(i+1, new_image.cols-1) = image.at<uchar>(i,image.cols-1);
-	}
-	for (int i=0;i<image.rows;i++)
-		for (int j = 0; j < image.cols; j++) 
-		{
-			new_image.at<uchar>(i + 1, j + 1) = image.at<uchar>(i, j);
-		}
-
-	return new_image;
-}
-int main() 
-{
-	Mat image = imread("D:\\GitProject\\parallel-programming-1\\1706-4\\dobrohotov_vn\\Lab 1\\Picture\\cat.jpg");
-	if (!image.data) {
-		cout << "Error load image" << endl;
-		system("pause");
-		return -1;
-	}
-	double kernel[9] = { 1,2,1,2,4,2,1,2,1 };
-	for (int i = 0; i < 9; ++i)
-		kernel[i] /= 16;
-	/*double *kernel = new double[9];
-	kernel= createKernel(kernel, 10);
-	for (int i = 0; i < 9; i++)
-		cout << kernel[i]<<"  ";*/
-	cvtColor(image, image, COLOR_BGR2GRAY);
-//	threshold(image, image, 100, 255, THRESH_BINARY);
-	namedWindow("image", WINDOW_NORMAL);
-	namedWindow("Copy", WINDOW_NORMAL);
-	imshow("image", image);
-	Mat new_image=duplicateBorder(image);
-	/*for (int i = 0; i < new_image.rows; i++)
-	{
-		for (int j = 0; j < new_image.cols; j++) 
-		{
-			cout <<(int) new_image.at<uchar>(i, j) << " ";
-		}
-		cout << endl;
-	}*/
-	uchar *data = new_image.data;
-	uchar *res_data = new uchar[image.rows*image.cols];
-	for (int i = 0; i < image.rows*image.cols; i++)
-		res_data[i] = 0;
-
-	int count_res_data=0;
-	int move = 0;
-	cout << new_image.rows*new_image.cols - 2*new_image.cols - 1 << " ind" << endl;
-	for (int i = 0; i < new_image.rows*new_image.cols-2*new_image.cols-1; i++) 
-	{
-		int res = 0;
-		move = 0;
-		//cout << i;
-		int count_kernel=0;
-		for (int j = 0; j < 3; j++) 
-		{
-			for (int k = 0; k < 3; k++)
-			{
-				//cout <<"   "<<(int)new_image.data[i + j + k + move] << " ";
-				res += new_image.data[i + j + k + move] * kernel[count_kernel];
-				count_kernel++;
-			}
-			move += new_image.cols-1;
-		}
-		if (res > 255)
-			res = 255;
-		if (res < 0)
-			res = 0;
-		res_data[count_res_data] = res;
-		count_res_data++;
-		if ((i + 3) % new_image.cols == 0)
-			i += 2;
-	}
-	Mat res(image.rows, image.cols, CV_8UC1);
-	res.data = res_data;
-	namedWindow("res", WINDOW_NORMAL);
-	imshow("Copy", new_image);
-	imshow("res", res);
-	waitKey(0);
-	return 0;
-}  
-//9232256
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include <mpi.h>
+//using namespace cv;
+//using namespace std;
+//#define M_PI 3.14159265358979323846
+//Mat filterGauss(Mat image)
+//{
+//	return image;
+//}
+//
+//double* createKernel(double* kernel, double sigma)
+//{
+//	double sum = 0;
+//	for (int i = -1; i <= 1; i++)
+//	{
+//		for (int j = -1; j <= 1; j++)
+//		{
+//			kernel[i + j + 2 * (i + 2)] = (double)(exp(-(i*i + j * j) / (sigma*sigma))) *(1/ (sigma*sqrt(2 * M_PI)));
+//			sum += kernel[i + j + 2 * (i + 2)];
+//		}
+//	}
+//	for (int i = -1; i <= 1; i++) {
+//		for (int j = -1; j <= 1; j++) {
+//			kernel[i + j + 2 * (i + 2)] /= sum;
+//		}
+//	}
+//	return kernel;
+//}
+//
+//Mat duplicateBorder(Mat image) 
+//{
+//
+//	Mat new_image(image.rows + 2, image.cols + 2, CV_8UC1);
+//	for (int i = 0; i < new_image.rows; i++)
+//		for (int j = 0; j < new_image.cols; j++)
+//			new_image.at<uchar>(i, j) = 128;
+//	new_image.at<uchar>(0, 0) = image.at<uchar>(0, 0);
+//	new_image.at<uchar>(0, new_image.cols-1) = image.at<uchar>(0, image.cols-1);
+//	new_image.at<uchar>(new_image.rows-1, 0) = image.at<uchar>(image.rows-1, 0);
+//	new_image.at<uchar>(new_image.rows-1,new_image.cols-1) = image.at<uchar>(image.rows-1, image.cols-1);
+//	for (int i = 0; i < image.cols; i++)
+//	{
+//		new_image.at<uchar>(0, i + 1) = image.at<uchar>(0, i);
+//		new_image.at<uchar>(new_image.rows - 1, i + 1) = image.at<uchar>(image.rows - 1, i);
+//	}
+//	for (int i = 0; i < image.rows; i++)
+//	{
+//		new_image.at<uchar>(i+1, 0) = image.at<uchar>(i, 0);
+//		new_image.at<uchar>(i+1, new_image.cols-1) = image.at<uchar>(i,image.cols-1);
+//	}
+//	for (int i=0;i<image.rows;i++)
+//		for (int j = 0; j < image.cols; j++) 
+//		{
+//			new_image.at<uchar>(i + 1, j + 1) = image.at<uchar>(i, j);
+//		}
+//
+//	return new_image;
+//}
+//uchar*  processImage(uchar* data,double* kernel, int rows, int cols) 
+//{
+//	uchar *res_data = new uchar[(rows-2)*(cols-2)];
+//	int count_res_data = 0;
+//	int move = 0;
+//	cout << (rows)*(cols) - 2 * (cols) - 1 << " ind" << endl;
+//	for (int i = 0; i < (rows)*(cols) - 2 * (cols) - 1; i++)
+//	{
+//		int res = 0;
+//		move = 0;
+//		//cout << i;
+//		int count_kernel = 0;
+//		for (int j = 0; j < 3; j++)
+//		{
+//			for (int k = 0; k < 3; k++)
+//			{
+//				//cout <<"   "<<(int)new_image.data[i + j + k + move] << " ";
+//				res += data[i + j + k + move] * kernel[count_kernel];
+//				count_kernel++;
+//			}
+//			move += (cols) - 1;
+//		}
+//		if (res > 255)
+//			res = 255;
+//		if (res < 0)
+//			res = 0;
+//		res_data[count_res_data] = res;
+//		count_res_data++;
+//		if ((i + 3) % (cols) == 0)
+//			i += 2;
+//	}
+//	return res_data;
+//}
+//int main() 
+//{
+//	Mat image = imread("D:\\GitProject\\parallel-programming-1\\1706-4\\dobrohotov_vn\\Lab 1\\Picture\\cat.jpg");
+//	if (!image.data) {
+//		cout << "Error load image" << endl;
+//		system("pause");
+//		return -1;
+//	}
+//	double kernel[9] = { 1,2,1,2,4,2,1,2,1 };
+//	for (int i = 0; i < 9; ++i)
+//		kernel[i] /= 16;
+//	/*double *kernel = new double[9];
+//	kernel= createKernel(kernel, 10);
+//	for (int i = 0; i < 9; i++)
+//		cout << kernel[i]<<"  ";*/
+//	cvtColor(image, image, COLOR_BGR2GRAY);
+////	threshold(image, image, 100, 255, THRESH_BINARY);
+//	namedWindow("image", WINDOW_NORMAL);
+//	namedWindow("Copy", WINDOW_NORMAL);
+//	imshow("image", image);
+//	Mat new_image=duplicateBorder(image);
+//	/*for (int i = 0; i < new_image.rows; i++)
+//	{
+//		for (int j = 0; j < new_image.cols; j++) 
+//		{
+//			cout <<(int) new_image.at<uchar>(i, j) << " ";
+//		}
+//		cout << endl;
+//	}*/
+//	uchar *data = new_image.data;
+//	//uchar *res_data = new uchar[image.rows*image.cols];
+//	//int count_res_data=0;
+//	//int move = 0;
+//	//cout << new_image.rows*new_image.cols - 2*new_image.cols - 1 << " ind" << endl;
+//	//for (int i = 0; i < new_image.rows*new_image.cols-2*new_image.cols-1; i++) 
+//	//{
+//	//	int res = 0;
+//	//	move = 0;
+//	//	//cout << i;
+//	//	int count_kernel=0;
+//	//	for (int j = 0; j < 3; j++) 
+//	//	{
+//	//		for (int k = 0; k < 3; k++)
+//	//		{
+//	//			//cout <<"   "<<(int)new_image.data[i + j + k + move] << " ";
+//	//			res += new_image.data[i + j + k + move] * kernel[count_kernel];
+//	//			count_kernel++;
+//	//		}
+//	//		move += new_image.cols-1;
+//	//	}
+//	//	if (res > 255)
+//	//		res = 255;
+//	//	if (res < 0)
+//	//		res = 0;
+//	//	res_data[count_res_data] = res;
+//	//	count_res_data++;
+//	//	if ((i + 3) % new_image.cols == 0)
+//	//		i += 2;
+//	//}
+//	//Mat res(image.rows, image.cols, CV_8UC1);
+//	Mat res(image.rows, image.cols, CV_8UC1);
+//	res.data = processImage(new_image.data,kernel,new_image.rows,new_image.cols);
+//	namedWindow("res", WINDOW_NORMAL);
+//	imshow("Copy", new_image);
+//	imshow("res", res);
+//	waitKey(0);
+//	return 0;
+//}  
+////9232256
