147a220a655d34750cdc91906313d75f0ff32b1c
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2020-04-18 18:40:16 +0300

    finish openmp version

--- 1706-4/dobrohotov_vn/Lab 1/Lab 1/Source.cpp
+++ 1706-4/dobrohotov_vn/Lab 1/Lab 1/Source.cpp
@@ -1,201 +1,151 @@
 #include <iostream>
-#include <iomanip>
-#include <mpi.h>
+#include <string>
+#include <chrono>
+#include <omp.h>
 
-using namespace std;
+#include <opencv2/opencv.hpp>
 
-int* InitMatrix(int rows, int cols)
-{
-	int* vec = new int[rows*cols];
-	if (rows < 2 && cols < 2)
-		for (int i = 0; i < rows*cols; i++)
-			cin >> vec[i];
-	else
-		for (int i = 0; i < rows*cols; i++)
-			vec[i] = rand() % 10000;
-	return vec;
-}
-void PrintVector(int *x, int size)
-{
-	if (size < 50)
-		for (int i = 0; i < size; i++)
-			cout << x[i] << "  ";
-	else
-		cout << "Vector too large" << endl;
-}
-void PrintInfo(int rank, int size, int block, int rows, int cols, int *tmp, int *local_max)
-{
-	cout << "\nInformation\nProcess " << rank << "\n" << "Size " << size << "\n" << "block " << block << "\n" << "rows " << rows << "\n" << "cols " << cols << endl;
+#define M_PI 3.14159265358979323846
 
-	for (int i = 0; i < rows*block; i++)
-		cout << " tmp " << rank << " " << tmp[i] << " ";
-	for (int i = 0; i < block; i++)
-		cout << "local max " << rank << " " << local_max[i] << " ";
-}
-void PrintMatrix(int *vec, int rows, int cols)
+double* Kernel()
 {
-	if (rows < 100 && cols < 100) {
-		cout << vec[0] << setw(2) << "  ";
-		for (int i = 1; i < rows*cols; i++)
-		{
-			cout << setw(2) << vec[i];
-			(i % (cols)) == cols - 1 ? cout << endl : cout << "  ";
-		}
-		cout << endl;
-	}
-	else
-		cout << "Matrix too large" << endl;
+    double kernel[9] = { 1,2,1,2,4,2,1,2,1 };
+    //normirovvka intensity
+    for (int i = 0; i < 9; ++i)
+        kernel[i] /= 16;
+    return  kernel;
 }
-void Tranpose(int* vec, int rows, int cols)
+double* createKernel(double* kernel, double sigma)
 {
-	int* copy = new int[rows*cols];
-	for (int i = 0; i < rows*cols; i++)
-		copy[i] = vec[i];
-	int ind = 0;
-	for (int i = 0; i < cols; i++) {
-		int n = 0;
-		for (int j = 0; j < rows; j++) {
-			vec[ind] = copy[i + n];
-			ind++;
-			n += cols;
-		}
-	}
-	delete[] copy;
+    double sum = 0;
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            kernel[i + j + 2 * (i + 2)] = (double)(exp(-(i*i + j * j) / (sigma*sigma))) *(1 / (sigma*sqrt(2 * M_PI)));
+            sum += kernel[i + j + 2 * (i + 2)];
+        }
+    }
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            kernel[i + j + 2 * (i + 2)] /= sum;
+        }
+    }
+    return kernel;
+}
+void PrintData(cv::Mat image)
+{
+    std::cout << std::endl;
+    for (int i = 0; i < image.rows*image.cols; ++i)
+    {
+        if (i % image.cols == 0 && i != 0)
+            std::cout << std::endl;
+        std::cout << static_cast<int>(image.data[i]) << "  ";
+    }
+}
+void PrintMat(cv::Mat image)
+{
+    for (int i = 0; i < image.rows; i++)
+    {
+        for (int j = 0; j < image.cols; j++)
+        {
+            std::cout << (int)image.at<uchar>(i, j) << "  ";
+        }
+        std::cout << std::endl;
+    }
 }
 
-void Max(int *mas, int *res, int cols, int pos)
+int Clamp(int num)
 {
-	int max = mas[0];
-	for (int i = 1; i < cols; i++)
-	{
-		if (max < mas[i])
-			max = mas[i];
-	}
-	res[pos] = max;
+    if (num > 255)
+        return 255;
+    else if (num < 0)
+        return 0;
+    return num;
+
+}
+cv::Mat duplicateBorder(cv::Mat image)
+{
+    cv::Mat new_image(image.rows + 2, image.cols + 2, CV_8UC1);
+    new_image.at<uchar>(0, 0) = image.at<uchar>(0, 0);
+    new_image.at<uchar>(0, new_image.cols - 1) = image.at<uchar>(0, image.cols - 1);
+    new_image.at<uchar>(new_image.rows - 1, 0) = image.at<uchar>(image.rows - 1, 0);
+    new_image.at<uchar>(new_image.rows - 1, new_image.cols - 1) = image.at<uchar>(image.rows - 1, image.cols - 1);
+    for (int i = 0; i < image.cols; i++)
+    {
+        new_image.at<uchar>(0, i + 1) = image.at<uchar>(0, i);
+        new_image.at<uchar>(new_image.rows - 1, i + 1) = image.at<uchar>(image.rows - 1, i);
+    }
+    for (int i = 0; i < image.rows; i++)
+    {
+        new_image.at<uchar>(i + 1, 0) = image.at<uchar>(i, 0);
+        new_image.at<uchar>(i + 1, new_image.cols - 1) = image.at<uchar>(i, image.cols - 1);
+    }
+    for (int i = 0; i < image.rows; i++)
+        for (int j = 0; j < image.cols; j++)
+        {
+            new_image.at<uchar>(i + 1, j + 1) = image.at<uchar>(i, j);
+        }
+    return new_image;
 }
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
-	int size; //количесво процессов
-	int rank; //индентификатор процесса
-	int rows, cols; //строкиб колонки
-	double start_linear = 0, end_linear = 0,
-		start_pp = 0, end_pp = 0;
-	int* vec = NULL; //Матрица в виде вектора
-	int* resl = NULL; //Результирующий вектор максимумов для последовательного
-	int *resp;//=NULL; //= NULL; //Результирующий вектор максимумов для параллейного
-	int *tmp = NULL; //Локальный массив для блока
-	int *local_max = NULL; //здесь будут храниться локальные максимумы
-	int block; //Количество столбцов в блоке
-	int left; //Остаточные блоки 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	if (size < 1) {
-		cout << "Error count process" << endl;
-		return -2;
-	}
-	if (rank == 0) {
-		//Input Matrix
-		while (1) {
-			cout << "Input rows and cols" << endl;
-			cin >> rows;
-			cin >> cols;
-			if (cols < 1 || rows < 1)
-			{
-				system("cls");
-				cout << "rows and cols must be >0" << endl;
-			}
-			else
-				break;
-		}
-		//Initialize
-		if (cols < size) //Если колонок меньше чем процессов, не задействовать все процессы, а столько сколько нужно
-			size = cols;
-		vec = InitMatrix(rows, cols); //инциализируется матрица в виде вектора рандомно
-		PrintMatrix(vec, rows, cols);
-		Tranpose(vec, rows, cols); //Транспонирование матрицы (переделывание вектора)
-		block = cols / size; //в блоке n-ое кол во столбцов
-		left = cols % size;  //остаточные столбцы которые не попадут на процессы
-		tmp = new int[rows*block];	// промежуточный массив для частей вектора
-		local_max = new int[block];
 
-		//Linear algorithm
-		resl = new int[cols];	// массив в котором будут хранится максимальные значения линейного алгоритма
-		start_linear = MPI_Wtime();
-		int max = vec[0];
-		int k = 0;
-		for (int i = 1; i < rows*cols; i++)
-		{
-			if (max < vec[i])
-				max = vec[i];
-			if (i % rows == rows - 1) {
-				resl[k] = max;
-				k++;
-				max = vec[++i];
-			}
-		}
-		end_linear = MPI_Wtime();
-	}
-	//parellel algorithm
-	start_pp = MPI_Wtime();
-	MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&block, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	resp = new int[cols];	// массив в котором будут хранится максимальные значения
-	if (rank > 0)
-	{
-		tmp = new int[rows*block];	// промежуточный массив для частей вектора
-		local_max = new int[block];
-	}
-	MPI_Scatter(vec, rows*block, MPI_INT, tmp, rows*block, MPI_INT, 0, MPI_COMM_WORLD);
-	//Поиск максимума по столбцам и занесение его в массив локальных сумм
-	int c = 0; //Смещение
-	for (int i = 0; i < block; i++)
-	{
-		int max = tmp[0 + c];
-		for (int j = 1; j < rows; j++)
-		{
-			if (tmp[j + c] > max)
-				max = tmp[j + c];
-		}
-		c += rows;
-		local_max[i] = max;
-	}
-	MPI_Gather(local_max, block, MPI_INT, resp, block, MPI_INT, 0, MPI_COMM_WORLD);
-	//Считаются столбцы которые не попали на процессы
-	if (rank == 0)
-		if (left != 0)
-		{
-			cout << "i am in left" << endl;
-			int c = 0; //Смещение
-			for (int i = 0; i < left; i++)
-			{
-				int max = vec[rows*size*block + c];
-				for (int j = 1; j < rows; j++)
-				{
-					if (vec[j + rows * size * block + c] > max)
-						max = vec[j + rows * size *block + c];
-				}
-				c += rows;
-				resp[i + size*block] = max;
-			}
-		}
-	end_pp = MPI_Wtime();
-	if (rank == 0) 
-	{
-		PrintVector(resl,cols);
-		cout << endl;
-		PrintVector(resp, cols);
-		cout << endl;
-		cout << "time linear  " << (end_linear - start_linear) * 1000 << endl;
-		cout << "time parallel  " << (end_pp - start_pp) * 1000 << endl;
-	}
-	delete[]vec;
-	delete[]resl;
-	//delete[]resp;
-	delete[] tmp;
-	delete[] local_max;
-	MPI_Finalize();
+    std::string path_to_image;
+    cv::Mat original, duplicate;
+    double kernel[3][3] = { {1,2,1},{ 2,4,2},{1,2,1} };
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            kernel[i][j] /= 16;
+    std::cout << argc << " " << argv[0] << " " << std::endl;
+    if (argc < 2)
+        path_to_image += "../Image/big.jpg";
+    else
+        path_to_image += argv[1];
+    original = cv::imread(path_to_image);
+    if (!original.data)
+    {
+        std::cout << "Error load image" << std::endl;
+        return -1;
+    }
+    if (original.channels() > 1)
+    {
+        cv::cvtColor(original, original, cv::COLOR_BGR2GRAY);
+        std::cout << "image to grey color" << std::endl;
+    }
+    cv::Mat  filter(original.rows, original.cols, CV_8UC1);
+    for (int i = 0; i < filter.rows; ++i)
+        for (int j = 0; j < filter.rows; ++j)
+            filter.at<uchar>(i, j) = 0;
+    namedWindow("Original", cv::WINDOW_NORMAL);
+    namedWindow("Duplicate", cv::WINDOW_NORMAL);
+    namedWindow("Filter", cv::WINDOW_NORMAL);
+    duplicate = duplicateBorder(original);
+    //Linear alghorithm
+
+    auto begin = std::chrono::steady_clock::now();
+#pragma omp parallel
+    {
+    #pragma omp for
+        for (int x = 1; x < duplicate.cols - 1; ++x)
+            for (int y = 1; y < duplicate.rows - 1; ++y)
+            {
+                int tmp = 0;
+                for (int i = -1; i <= 1; ++i)
+                    for (int j = -1; j <= 1; ++j)
+                    {
+                        tmp += (static_cast <int>(duplicate.at<uchar>(y + i, x + i))) * kernel[1 + i][1 + j];
+                    }
+                filter.at<uchar>(y - 1, x - 1) = (uchar)Clamp(tmp);
+            }
+    }
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    std::cout << "The time: " << elapsed_ms.count() << " ms\n";
+    cv::imshow("Original", original);
+    cv::imshow("Duplicate", duplicate);
+    cv::imshow("Filter", filter);
+    cv::waitKey();
+    return 0;
 }
