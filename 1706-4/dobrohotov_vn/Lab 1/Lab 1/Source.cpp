1b6c1cde1f7500204d90189d8122462973fe3506
Dybbor <19dobrohotov99@mail.ru>
2020-05-14 01:44:02 +0300

    lab done, but need to add check function

--- 1706-4/dobrohotov_vn/Lab 1/Lab 1/Source.cpp
+++ 1706-4/dobrohotov_vn/Lab 1/Lab 1/Source.cpp
@@ -1,59 +1,11 @@
 #include <iostream>
-#include <string>
-#include <chrono>
-#include <omp.h>
-
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <tbb/tbb.h>
+#include <tbb/blocked_range.h>
+#include <tbb/tick_count.h>
 
 #define M_PI 3.14159265358979323846
-
-double* Kernel()
-{
-    double kernel[9] = { 1,2,1,2,4,2,1,2,1 };
-    //normirovvka intensity
-    for (int i = 0; i < 9; ++i)
-        kernel[i] /= 16;
-    return  kernel;
-}
-double* createKernel(double* kernel, double sigma)
-{
-    double sum = 0;
-    for (int i = -1; i <= 1; i++)
-    {
-        for (int j = -1; j <= 1; j++)
-        {
-            kernel[i + j + 2 * (i + 2)] = (double)(exp(-(i*i + j * j) / (sigma*sigma))) *(1 / (sigma*sqrt(2 * M_PI)));
-            sum += kernel[i + j + 2 * (i + 2)];
-        }
-    }
-    for (int i = -1; i <= 1; i++) {
-        for (int j = -1; j <= 1; j++) {
-            kernel[i + j + 2 * (i + 2)] /= sum;
-        }
-    }
-    return kernel;
-}
-void PrintData(cv::Mat image)
-{
-    std::cout << std::endl;
-    for (int i = 0; i < image.rows*image.cols; ++i)
-    {
-        if (i % image.cols == 0 && i != 0)
-            std::cout << std::endl;
-        std::cout << static_cast<int>(image.data[i]) << "  ";
-    }
-}
-void PrintMat(cv::Mat image)
-{
-    for (int i = 0; i < image.rows; i++)
-    {
-        for (int j = 0; j < image.cols; j++)
-        {
-            std::cout << (int)image.at<uchar>(i, j) << "  ";
-        }
-        std::cout << std::endl;
-    }
-}
 
 int Clamp(int num)
 {
@@ -62,8 +14,23 @@ int Clamp(int num)
     else if (num < 0)
         return 0;
     return num;
-
 }
+
+void gauss_for_tbb(cv::Mat *src, cv::Mat *dst, double kernel[3][3], int number_col) 
+{
+    for (int y = 1; y < src->rows - 1; ++y)
+    {
+        int tmp = 0;
+        for (int i = -1; i <= 1; ++i)
+            for (int j = -1; j <= 1; ++j)
+            {
+                tmp += (static_cast <int>(src->at<uchar>(y + i, number_col + i))) * kernel[1 + i][1 + j];
+            }
+        dst->at<uchar>(y - 1, number_col - 1) = (uchar)Clamp(tmp);
+    }
+        
+}
+
 cv::Mat duplicateBorder(cv::Mat image)
 {
     cv::Mat new_image(image.rows + 2, image.cols + 2, CV_8UC1);
@@ -89,21 +56,16 @@ cv::Mat duplicateBorder(cv::Mat image)
     return new_image;
 }
 
-int main(int argc, char** argv)
+int main() 
 {
-
-    std::string path_to_image;
+    std::string path_to_image="../Image/big.jpg";
     cv::Mat original, duplicate;
     double kernel[3][3] = { {1,2,1},{ 2,4,2},{1,2,1} };
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
             kernel[i][j] /= 16;
-    std::cout << argc << " " << argv[0] << " " << std::endl;
-    if (argc < 2)
-        path_to_image += "../Image/big.jpg";
-    else
-        path_to_image += argv[1];
     original = cv::imread(path_to_image);
+    cv::Mat filter(original.rows, original.cols, CV_8UC1);
     if (!original.data)
     {
         std::cout << "Error load image" << std::endl;
@@ -114,35 +76,21 @@ int main(int argc, char** argv)
         cv::cvtColor(original, original, cv::COLOR_BGR2GRAY);
         std::cout << "image to grey color" << std::endl;
     }
-    cv::Mat  filter(original.rows, original.cols, CV_8UC1);
-    for (int i = 0; i < filter.rows; ++i)
-        for (int j = 0; j < filter.rows; ++j)
-            filter.at<uchar>(i, j) = 0;
-    namedWindow("Original", cv::WINDOW_NORMAL);
-    namedWindow("Duplicate", cv::WINDOW_NORMAL);
-    namedWindow("Filter", cv::WINDOW_NORMAL);
+    cv::namedWindow("Original", cv::WINDOW_NORMAL);
+    cv::namedWindow("Duplicate", cv::WINDOW_NORMAL);
+    cv::namedWindow("Filter", cv::WINDOW_NORMAL);
     duplicate = duplicateBorder(original);
-    //Linear alghorithm
-
-    auto begin = std::chrono::steady_clock::now();
-#pragma omp parallel
+    tbb::tick_count start = tbb::tick_count::now();
+    tbb::task_scheduler_init init();
+    tbb::parallel_for(tbb::blocked_range<size_t>(1, duplicate.cols-1), [&duplicate, &filter, &kernel](const tbb::blocked_range<size_t>& r)
     {
-    #pragma omp for
-        for (int x = 1; x < duplicate.cols - 1; ++x)
-            for (int y = 1; y < duplicate.rows - 1; ++y)
-            {
-                int tmp = 0;
-                for (int i = -1; i <= 1; ++i)
-                    for (int j = -1; j <= 1; ++j)
-                    {
-                        tmp += (static_cast <int>(duplicate.at<uchar>(y + i, x + i))) * kernel[1 + i][1 + j];
-                    }
-                filter.at<uchar>(y - 1, x - 1) = (uchar)Clamp(tmp);
-            }
-    }
-    auto end = std::chrono::steady_clock::now();
-    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    std::cout << "The time: " << elapsed_ms.count() << " ms\n";
+        for (size_t i = r.begin(); i != r.end(); ++i)
+        {
+            gauss_for_tbb(&duplicate, &filter, kernel,i);
+        }
+    });
+    tbb::tick_count finish = tbb::tick_count::now();
+    std::cout << (finish - start).seconds() << "sec" << std::endl;
     cv::imshow("Original", original);
     cv::imshow("Duplicate", duplicate);
     cv::imshow("Filter", filter);
